9e25984e1621621f4c02d8a5c6be324bf9d45478
Justin <justin@kaizenmarketing.io>
2023-03-25 12:37:46 -0300

    tweak routs

--- src/media/routes/mod.rs
+++ src/media/routes/mod.rs
@@ -10,6 +10,7 @@ use base64::{Engine, engine::general_purpose};
 pub fn get_media_routes() -> Vec<Route> {
     routes![
         convert_base64_media,
+        upload,
     ]
 }
 
@@ -28,7 +29,7 @@ struct MediaConversionResponse {
     bucket_url: String
 }
 
-#[post("/", data = "<media_conversion_request>")]
+#[post("/base64/upload", data = "<media_conversion_request>")]
 async fn convert_base64_media(media_conversion_request: Json<MediaConversionRequest>) -> Json<MediaConversionResponse> {
     let client = Client::default();
     let encoded_media: Vec<&str> = media_conversion_request.base64.split(";base64,").collect();
@@ -44,4 +45,21 @@ async fn convert_base64_media(media_conversion_request: Json<MediaConversionRequ
     Json(MediaConversionResponse{
         bucket_url: object.media_link,
     })
+}
+
+#[derive(Debug)]
+#[derive(Deserialize, Serialize)]
+struct UploadRequest {
+    files: Vec<String>
+}
+
+#[derive(Debug)]
+#[derive(Deserialize, Serialize)]
+struct UploadResponse {
+    data: Vec<String>
+}
+#[post("/upload", data = "<upload_request>")]
+async fn upload(upload_request: Json<UploadRequest>) -> Json<UploadResponse> {
+    println!("{:?}", upload_request.into_inner());
+    Json(UploadResponse { data: vec!["bingo".to_owned()] })
 }
