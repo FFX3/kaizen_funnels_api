fd0957a261a5241d7683641451c7080cae8f7874
Justin <justin@kaizenmarketing.io>
2023-04-01 14:32:34 -0300

    created schema.rs for gatekeeper

--- migrations/gatekeeper/2023-04-01-160656_auth_setup/up.sql
+++ migrations/gatekeeper/2023-04-01-160656_auth_setup/up.sql
@@ -1,1 +1,16 @@
--- Your SQL goes here
+CREATE TABLE organizations (
+    id serial PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    database_name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    organization_id INT NOT NULL,
+    FOREIGN KEY (organization_id)
+        REFERENCES organizations(id)
+);
